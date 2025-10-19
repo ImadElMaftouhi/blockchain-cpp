8db4ea2f3c8a32bbf1a19b1fb8a3198acfa6f43b
ImadElMaftouhi <imad.elmaft.wrk@gmail.com>
2025-10-20 00:59:52 +0100

    pushed build

---
+++ blockchain/build/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain
+# Build directory: C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(ex_1_merkle_test "C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain/build/test_ex1_merkle.exe")
+set_tests_properties(ex_1_merkle_test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain/CMakeLists.txt;58;add_test;C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain/CMakeLists.txt;0;")
+add_test(ex2_pow_test "C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain/build/test_ex2_pow.exe")
+set_tests_properties(ex2_pow_test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain/CMakeLists.txt;59;add_test;C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain/CMakeLists.txt;0;")
+add_test(ex3_pos_test "C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain/build/test_ex3_pos.exe")
+set_tests_properties(ex3_pos_test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain/CMakeLists.txt;60;add_test;C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain/CMakeLists.txt;0;")
+add_test(ex4_complete_test "C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain/build/test_ex4_complete.exe")
+set_tests_properties(ex4_complete_test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain/CMakeLists.txt;61;add_test;C:/Users/imade/Documents/Professional/IASD/S3/Blockchain & applied security/atelier_1/blockchain-cpp/blockchain/CMakeLists.txt;0;")
